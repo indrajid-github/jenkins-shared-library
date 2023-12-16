f2f5761a603b071c74a52a8bba870aa587a234d0
henithvaibav <henithvaibav@gmail.com>
2023-12-16 12:59:58 +0530

    dockerbuild lib updated

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -2,7 +2,6 @@ def call(String image_name, String image_tag)
 {   
        sh """
         docker image build -t ${image_name} .
-        docker image tag ${image_name}:${image_tag}
-        docker image tag ${image_name}:latest
+        docker image tag ${image_name} ${image_name}:${image_tag}
        """
 }    

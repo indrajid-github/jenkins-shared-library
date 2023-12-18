0e565a3c9b933f71f25180f7194c2ff27cf404da
henithvaibav <henithvaibav@gmail.com>
2023-12-18 09:21:32 +0530

    trivy target corrected

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,7 @@
 def call(String image_name, String image_tag)
 {   
        sh """
-        trivy image ${image_name}:${image_tag} ${image_name}:latest > scan.txt
+        trivy image ${image_name}:${image_tag} > scan.txt
         cat scan.txt
        """
 }    

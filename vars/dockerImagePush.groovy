45f1627c9d8b797b8cea5ac5cfd7ca1dceaf0bcf
indrajith <mailtoindrajith@gmail.com>
2023-12-19 11:23:08 +0530

    aws username added

--- vars/dockerImagePush.groovy
+++ vars/dockerImagePush.groovy
@@ -8,13 +8,14 @@
         echo "#######Clearing Docker Image Post image push ##########"
         docker image rm ${image_name}:${image_tag}
         docker image rm ${image_name}:latest
+        aws ecr get-login-password --region ${region} | docker login --username Surya --password-stdin ${awsaccid}.dkr.ecr.${region}.amazonaws.com
     """
 }*/
 
 def call(String awsaccid, String region, String reponame)
 {
        sh """
-              aws ecr get-login-password --region ${region} | docker login --username Surya --password-stdin ${awsaccid}.dkr.ecr.${region}.amazonaws.com
+              aws ecr get-login-password --region ap-southeast-2 | docker login --username AWS --password-stdin 639323015973.dkr.ecr.ap-southeast-2.amazonaws.com
               docker push ${awsaccid}.dkr.ecr.${region}.amazonaws.com/${reponame}:latest
               
        """

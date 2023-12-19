847e71e370ff53f691c4614c56d4fd766f4d9171
indrajith <mailtoindrajith@gmail.com>
2023-12-19 10:24:59 +0530

    docker build and scan function updated for ECR

--- vars/dockerBuild.groovy
+++ vars/dockerBuild.groovy
@@ -1,7 +1,16 @@
-def call(String image_name, String image_tag)
-{   
+//def call(String image_name, String image_tag)
+//{   
+//       sh """
+//        docker image build -t ${image_name} .
+//        docker image tag ${image_name} ${image_name}:${image_tag}
+//       """
+//}
+
+
+def call(String awsaccid, String region, String reponame)
+{
        sh """
-        docker image build -t ${image_name} .
-        docker image tag ${image_name} ${image_name}:${image_tag}
+              docker build -t ${reponame} .
+              docker tag ${reponame}:latest ${awsaccid}.dkr.ecr.${region}.amazonaws.com/${reponame}:latest
        """
 }    
--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,7 +1,15 @@
-def call(String image_name, String image_tag)
+/*def call(String image_name, String image_tag)
 {   
        sh """
         trivy image ${image_name}:${image_tag} > scan.txt
         cat scan.txt
+       """
+}*/
+
+def call(String awsaccid, String region, String reponame)
+{
+       sh """
+              trivy image ${awsaccid}.dkr.ecr.${region}.amazonaws.com/${reponame}:latest > scan.txt
+              cat scan.txt
        """
 }    
